dd6fa2a5bd0cae08bc9a210cccbcb9292a3da2f3
КононовАА0302 <81182918+Zombiekillerss@users.noreply.github.com>
2022-04-11 19:18:30 +0300

    fix output of addresses

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,23 +4,25 @@ fun main() {
     val str = """
             1. 123, Название города 1, ул. Название улицы, д. Номер дома
             2. 124, Название города 2, ул. Название улицы1, д. Номер дома
-    """.trimIndent()
-    val addressList: MutableList<Address> = parseAddresses(str)
+    """
+    val addressList: List<Address> = parseAddresses(str)
     println("2. ${addressList[1].postcode}, ${addressList[1].city}, ул. ${addressList[1].street}, д. ${addressList[1].houseNumber}")
-    var maxPostcode = Int.MIN_VALUE
-    var minPostcode = Int.MAX_VALUE
-    var longNameStreet = addressList[0].street
-    var shortNameStreet = addressList[0].street
-    for(i in addressList.indices){
-        if(maxPostcode < addressList[i].postcode.toInt())
-            maxPostcode = addressList[i].postcode.toInt()
-        if(minPostcode > addressList[i].postcode.toInt())
-            minPostcode = addressList[i].postcode.toInt()
-        if(longNameStreet.length < addressList[i].street.length)
-            longNameStreet = addressList[i].street
-        if(shortNameStreet.length > addressList[i].street.length)
-            shortNameStreet = addressList[i].street
+    var maxPostcode = addressList[0]
+    var minPostcode = addressList[0]
+    var longNameStreet = addressList[0]
+    var shortNameStreet = addressList[0]
+    for (i in addressList.indices) {
+        if (maxPostcode.postcode.toInt() < addressList[i].postcode.toInt())
+            maxPostcode = addressList[i]
+        if (minPostcode.postcode.toInt() > addressList[i].postcode.toInt())
+            minPostcode = addressList[i]
+        if (longNameStreet.street.length < addressList[i].street.length)
+            longNameStreet = addressList[i]
+        if (shortNameStreet.street.length > addressList[i].street.length)
+            shortNameStreet = addressList[i]
     }
-    println("Max postcode: $maxPostcode, min postcode: $minPostcode.")
-    println("Shortest street name $shortNameStreet, longest street name $longNameStreet.")
+    println("Address with max postcode: ${maxPostcode.postcode}, ${maxPostcode.city}, ул. ${maxPostcode.street}, д. ${maxPostcode.houseNumber}.")
+    println("Address with min postcode: ${minPostcode.postcode}, ${minPostcode.city}, ул. ${minPostcode.street}, д. ${minPostcode.houseNumber}.")
+    println("Address with shortest street name ${shortNameStreet.postcode}, ${shortNameStreet.city}, ул. ${shortNameStreet.street}, д. ${shortNameStreet.houseNumber}.")
+    println("Address with longest street name ${longNameStreet.postcode}, ${longNameStreet.city}, ул. ${longNameStreet.street}, д. ${longNameStreet.houseNumber}.")
 }
