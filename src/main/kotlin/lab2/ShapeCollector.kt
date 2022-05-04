fd257be4db9ce5004e20e823b1467ca5e44f6768
КононовАА0302 <81182918+Zombiekillerss@users.noreply.github.com>
2022-05-04 22:44:25 +0300

    fix bugs

--- src/main/kotlin/lab2/ShapeCollector.kt
+++ src/main/kotlin/lab2/ShapeCollector.kt
@@ -1,48 +1,4 @@
-package lab1.lab2
-
-import kotlin.math.*
-
-class Figure(
-    override val borderColor: Color,
-    override val fillColor: Color,
-    val lengthFirstSide: Double = 0.0,
-    val lengthSecondSide: Double = 0.0,
-    val lengthThirdSide: Double = 0.0
-) : ColoredShape2d {
-    private var s: Double = 0.0
-    override fun calcArea(): Double {
-        if (!(borderColor.blue in 0..255 && borderColor.red in 0..255
-                    && borderColor.green in 0..255 && fillColor.blue in 0..255
-                    && fillColor.red in 0..255 && fillColor.green in 0..255
-                    && fillColor.transparency in 0..100 && borderColor.transparency in 0..100)
-        )
-            error(
-                "Wrong color values"
-            )
-        s = if (s == 0.0) {
-            if (lengthFirstSide > 0.0 && lengthSecondSide > 0.0 && lengthThirdSide > 0.0) {
-                if (lengthFirstSide + lengthSecondSide > lengthThirdSide
-                    && lengthFirstSide + lengthThirdSide > lengthSecondSide
-                    && lengthSecondSide + lengthThirdSide > lengthFirstSide
-                ) {
-                    val perimeter = (lengthFirstSide + lengthSecondSide + lengthThirdSide) / 2
-                    sqrt(
-                        perimeter * (perimeter - lengthFirstSide) * (perimeter - lengthSecondSide) * (perimeter - lengthThirdSide)
-                    )
-                } else error(("Invalid triangle sides"))
-            } else if (lengthFirstSide > 0.0 && lengthSecondSide > 0.0)
-                lengthFirstSide * lengthSecondSide
-            else if (lengthFirstSide > 0.0)
-                PI * lengthFirstSide.pow(2.0)
-            else error("All side values are 0 or some sides are less than 0")
-        } else s
-        return s
-    }
-
-    fun getS(): Double {
-        return s
-    }
-}
+package lab2
 
 class ShapeCollector {
     private var listFigure: MutableList<Figure> = mutableListOf()
@@ -53,25 +9,21 @@ class ShapeCollector {
         newFigure.calcArea()
         if (maxS == null)
             maxS = newFigure
-        else if (maxS!!.getS() < newFigure.getS())
+        else if (maxS!!.calcArea() < newFigure.calcArea())
             maxS = newFigure
         if (minS == null)
             minS = newFigure
-        else if (minS!!.getS() > newFigure.getS())
+        else if (minS!!.calcArea() > newFigure.calcArea())
             minS = newFigure
-        sum += newFigure.getS()
+        sum += newFigure.calcArea()
         listFigure.add(newFigure)
     }
 
     fun getFigureSmallest(): Figure {
-        if (minS == null)
-            error("You have not added a figure!")
         return minS as Figure
     }
 
     fun getFigureLargest(): Figure {
-        if (maxS == null)
-            error("You have not added a figure!")
         return maxS as Figure
     }
 
@@ -105,22 +57,13 @@ class ShapeCollector {
         return mapOf(fillColor to (listFigure.filter { it.fillColor == fillColor }))
     }
 
-    fun getListDifferentFigure(): List<List<Figure>> {
+    fun getListDifferentFigure(): List<List<Figure>>{
         return listFigure.let { figure ->
             listOf(
-                figure.filter {
-                    it.lengthFirstSide != 0.0 && it.lengthSecondSide == 0.0 && it.lengthThirdSide == 0.0
-                },
-                figure.filter {
-                    it.lengthFirstSide == it.lengthSecondSide && it.lengthSecondSide != 0.0 && it.lengthThirdSide == 0.0
-                },
-                figure.filter {
-                    it.lengthFirstSide != it.lengthSecondSide && it.lengthSecondSide != 0.0
-                            && it.lengthThirdSide == 0.0 && it.lengthFirstSide != 0.0
-                },
-                figure.filter {
-                    it.lengthFirstSide != 0.0 && it.lengthSecondSide != 0.0 && it.lengthThirdSide != 0.0
-                },
+                figure.filterIsInstance<Circle>(),
+                figure.filterIsInstance<Square>(),
+                figure.filterIsInstance<Rectangle>(),
+                figure.filterIsInstance<Triangle>(),
             )
         }
     }
