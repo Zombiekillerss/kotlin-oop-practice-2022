779abdded4c95a12290dc980c99a0a8d357f5a9e
КононовАА0302 <81182918+Zombiekillerss@users.noreply.github.com>
2022-05-04 22:44:59 +0300

    add interface Figure

--- src/main/kotlin/lab2/2dFigures.kt
+++ src/main/kotlin/lab2/2dFigures.kt
@@ -1,6 +1,13 @@
-package lab1.lab2
+package lab2
 
-data class Color(val red: Int = 0, val green: Int = 0, val blue: Int = 0, val transparency: Int = 0)
+data class Color(val red: Int = 0, val green: Int = 0, val blue: Int = 0, val transparency: Int = 0){
+    init {
+        require(red in 0 until 256) {"Incorrect red value entered"}
+        require(green in 0 until 256) {"Incorrect green value entered"}
+        require(blue in 0 until 256) {"Incorrect blue value enteredВведено неверное значение синего цвета"}
+        require(transparency in 0 until 101) {"Incorrect color transparency value entered"}
+    }
+}
 
 interface Shape2d {
     fun calcArea(): Double
@@ -10,3 +17,5 @@ interface ColoredShape2d : Shape2d {
     val borderColor: Color
     val fillColor: Color
 }
+
+interface Figure : ColoredShape2d
