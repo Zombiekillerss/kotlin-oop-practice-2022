b5074c73ba45e90bb63448425dcdb7716721c982
КононовАА0302 <81182918+Zombiekillerss@users.noreply.github.com>
2022-05-05 14:12:43 +0300

    fix bugs

--- src/test/kotlin/lab2/ShapeCollectorTest.kt
+++ src/test/kotlin/lab2/ShapeCollectorTest.kt
@@ -162,6 +162,13 @@ internal class ShapeCollectorTest {
         val figure2 = Circle(9.0,Color(),Color())
         val expected = figure1.calcArea() + figure2.calcArea()
         val collector = ShapeCollector()
+        try{
+            collector.getSumArea()
+            assert(false)
+        }catch(e: Exception){
+            val exception = "You have not added a figure!"
+            assertEquals(exception, e.message )
+        }
         collector.addFigure(figure1)
         collector.addFigure(figure2)
         assertEquals(expected, collector.getSumArea())
@@ -179,18 +186,14 @@ internal class ShapeCollectorTest {
     }
 
     @Test
-    fun findFillColor() {
+    fun findFillColorTest() {
         val figure1 = Circle(10.8,Color(),Color(1,2))
         val figure2 = Circle(9.0,Color(),Color())
         val expected = listOf<Figure>(figure1)
         val collector = ShapeCollector()
         collector.addFigure(figure1)
         collector.addFigure(figure2)
-        assertEquals(expected, collector.findBorderColor(Color(1,2)))
-    }
-
-    @Test
-    fun getCountFigure() {
+        assertEquals(expected, collector.findFillColor(Color(1,2)))
     }
 
     @Test
