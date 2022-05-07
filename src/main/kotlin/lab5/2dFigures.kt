c6a6e810e7fb9fc56571f44c795590e081d1f909
КононовАА0302 <81182918+Zombiekillerss@users.noreply.github.com>
2022-05-07 13:11:20 +0300

    add lab5

---
+++ src/main/kotlin/lab5/2dFigures.kt
@@ -0,0 +1,41 @@
+package lab5
+
+data class Color(val red: Int = 0, val green: Int = 0, val blue: Int = 0, val transparency: Int = 0){
+    init {
+        require(red in 0 until 256) {"Incorrect red value entered"}
+        require(green in 0 until 256) {"Incorrect green value entered"}
+        require(blue in 0 until 256) {"Incorrect blue value entered"}
+        require(transparency in 0 until 101) {"Incorrect color transparency value entered"}
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Color
+
+        if (red != other.red) return false
+        if (green != other.green) return false
+        if (blue != other.blue) return false
+        if (transparency != other.transparency) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = red
+        result = 31 * result + green
+        result = 31 * result + blue
+        result = 31 * result + transparency
+        return result
+    }
+}
+
+interface Shape2d {
+    fun calcArea(): Double
+}
+
+interface ColoredShape2d : Shape2d {
+    val borderColor: Color
+    val fillColor: Color
+}
---
+++ src/main/kotlin/lab5/ShapeCollector.kt
@@ -0,0 +1,72 @@
+package lab5
+
+class ShapeCollector<Figure: ColoredShape2d> {
+    private var listFigure: MutableList<Figure> = mutableListOf()
+    private var maxS: Figure? = null
+    private var minS: Figure? = null
+    private var sum: Double = 0.0
+    fun addFigure(newFigure: Figure) {
+        newFigure.calcArea()
+        if (maxS == null)
+            maxS = newFigure
+        else if (maxS!!.calcArea() < newFigure.calcArea())
+            maxS = newFigure
+        if (minS == null)
+            minS = newFigure
+        else if (minS!!.calcArea() > newFigure.calcArea())
+            minS = newFigure
+        sum += newFigure.calcArea()
+        listFigure.add(newFigure)
+    }
+
+    fun getFigureSmallest(): Figure? {
+        return minS
+    }
+
+    fun getFigureLargest(): Figure? {
+        return maxS
+    }
+
+    fun getSumArea(): Double {
+        require(sum != 0.0) { "You have not added a figure!" }
+        return sum
+    }
+
+    fun findBorderColor(borderColor: Color): List<Figure> {
+        return listFigure.filter { it.borderColor == borderColor } as MutableList<Figure>
+    }
+
+    fun findFillColor(fillColor: Color): List<Figure> {
+        return listFigure.filter { it.fillColor == fillColor } as MutableList<Figure>
+    }
+
+    fun getListFigure(): List<Figure> {
+        return listFigure
+    }
+
+    fun getCountFigure(): Int {
+        return listFigure.size
+    }
+
+    fun getBorderFigure(borderColor: Color): Map<Color, List<Figure>> {
+        return mapOf(borderColor to (listFigure.filter { it.borderColor == borderColor }))
+    }
+
+    fun getFillFigure(fillColor: Color): Map<Color, List<Figure>> {
+        return mapOf(fillColor to (listFigure.filter { it.fillColor == fillColor }))
+    }
+
+    fun addAll(newListFigure: Collection<Figure>){
+        listFigure.addAll(newListFigure)
+    }
+
+    fun getSorted(newComparator: Comparator<Figure>): List<Figure>{
+        val sortList = listFigure
+        sortList.sortWith(newComparator)
+        return sortList
+    }
+
+    fun getListDifferentFigure(): Map<Class<Any>, List<Figure>> {
+        return listFigure.groupBy { it.javaClass }
+    }
+}
