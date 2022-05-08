f218139884f617345ab1af063b5764107e0bc583
КононовАА0302 <81182918+Zombiekillerss@users.noreply.github.com>
2022-05-08 16:07:57 +0300

    fix bugs

--- src/test/kotlin/lab6/ListFigureFileTest.kt
+++ src/test/kotlin/lab6/ListFigureFileTest.kt
@@ -14,7 +14,8 @@ internal class ListFigureFileTest {
         val fileColl = ListFigureFile()
         collector.addFigure(Triangle(1.2, 2.9, 3.0, Color(), Color()))
         fileColl.serializationToFile(collector.getListFigure(), "src/main/kotlin/lab6/save.txt")
-        val expected = "[{\"type\":\"lab6.Circle\",\"radius\":2.0,\"borderColor\":{},\"fillColor\":{},\"s\":12.566370614359172},{\"type\":\"lab6.Rectangle\",\"lengthFirstSide\":2.0,\"lengthSecondSide\":2.0,\"borderColor\":{},\"fillColor\":{},\"s\":4.0},{\"type\":\"lab6.Triangle\",\"lengthFirstSide\":1.2,\"lengthSecondSide\":2.9,\"lengthThirdSide\":3.0,\"borderColor\":{},\"fillColor\":{},\"s\":1.7269753182949656}]"
+        val expected =
+            "[{\"type\":\"lab6.Circle\",\"radius\":2.0,\"borderColor\":{},\"fillColor\":{},\"s\":12.566370614359172},{\"type\":\"lab6.Rectangle\",\"lengthFirstSide\":2.0,\"lengthSecondSide\":2.0,\"borderColor\":{},\"fillColor\":{},\"s\":4.0},{\"type\":\"lab6.Triangle\",\"lengthFirstSide\":1.2,\"lengthSecondSide\":2.9,\"lengthThirdSide\":3.0,\"borderColor\":{},\"fillColor\":{},\"s\":1.7269753182949656}]"
         assertEquals(expected, File("src/main/kotlin/lab6/save.txt").readText())
     }
 
